34102e8b56af53cb4fb21c54c77b50f152bbc025
deewooley <55896326+deewooley@users.noreply.github.com>
2020-01-11 23:44:38 -0500

    updated code from additonal practice exercises

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+"use strict";
 // alert("My first line of javascript code ...yay it works!");
 // console.log(3+2);
 //-----
-// "use strict";
+//"use strict";
 // let message = "hello world";
 // message = "Wakanda";
 // console.log(message);
@@ -110,19 +111,245 @@
 // alert(isBoss);
 //-----
 
-"use strict";
-let accessAllowed;
-let age = prompt("How old are you",'');
+// function showMessage(from, text) {
 
-if (age >18){
-    accessAllowed = true;
-}
-else {
-    accessAllowed = false;
-}
-alert(accessAllowed);
-}
+//     from = '*' + from + '*'; // make "from" look nicer
+  
+//     alert( from + ': ' + text );
+//   }
+  
+//   let from = "Ann";
+  
+//   showMessage(from, "Hello"); // *Ann*: Hello
+  
+  // the value of "from" is the same, the function modified a local copy
+//   alert( from ); // Ann
 
-//JS Findamentals and Objects: the basics
 
-11-16
+
+
+//SOLO WORK STARTS HERE
+
+//Homework HERE
+//-------------
+//Create a web-page that asks for a name and outputs it. 
+
+// "use strict";
+// let name = prompt("What is your name?");
+// console.log(name);
+//-------------
+
+
+
+
+
+// "use strict";
+// let year = prompt("In which year was ECMAS...");
+// if (year == 2015) {
+//     alert ("You are right!"); 
+//     alert ("Great Job");
+// }
+// else(alert ('No did you even study?'));
+
+//-------
+
+// let accessAllowed = Boolean;
+// let age = prompt("How old are you?");
+// if (age>18){
+//     accessAllowed = true;
+
+// }
+// else{
+//     accessAllowed = false;
+// }
+// alert(accessAllowed);
+
+//--------
+
+//? used as a replacement for if
+
+// let company = prompt('Which company created JavaScript?');
+// (company == 'Netscape') ?
+// alert('Right!') : alert('Wrong.');
+
+
+//?=if
+//: = else
+
+//--------
+
+
+// let result;
+
+// let number = prompt('Enter a Number');
+
+// (number<4) ?
+//     alert ('Below') : alert ('Above');
+
+    //-------
+
+
+    //  REMEMBER THIS!!!!!!!!
+
+//     let message = (login == 'Employee') ? 'Hello' :
+//   (login == 'Director') ? 'Greetings' :
+//   (login == '') ? 'No login' :
+//   '';
+
+  //----------
+
+//   let x;
+
+// false || (x = 1);
+
+// alert(x); // 1
+
+
+
+
+//Switch Statements
+
+// let a = 2 + 3;
+//ie switched bestween cases given a start point without the break
+// switch (a) {
+//   case 3:
+//     alert( 'Too small' );
+//   //  break;
+//   case 4:
+//     alert( 'Exactly!' );
+//   //  break;
+//   case 5:
+//     alert( 'Too large' );
+//   //  break;
+//   default:
+//     alert( "I don't know such values" );
+// }
+//---
+
+// switch (browser) {
+//     case 'Edge':
+//       alert( "You've got the Edge!" );
+//       break;
+  
+//     case 'Chrome':
+//     case 'Firefox':
+//     case 'Safari':
+//     case 'Opera':
+//       alert( 'Okay we support these browsers too' );
+//       break;
+  
+//     default:
+//       alert( 'We hope that this page looks ok!' );
+//   }
+
+//   if(browser == 'Edge') {
+//     alert("You've got the Edge!");
+//   } else if (browser == 'Chrome'
+//    || browser == 'Firefox'
+//    || browser == 'Safari'
+//    || browser == 'Opera') {
+//     alert( 'Okay we support these browsers too' );
+//   } else {
+//     alert( 'We hope that this page looks ok!' );
+//   }
+
+// //------
+
+//   let a = "1";
+// let b = 0;
+
+// switch (+a) {
+//   case b + 1:
+//     alert("this runs, because +a is 1, exactly equals b+1");
+//     break;
+
+//   default:
+//     alert("this doesn't run");
+// }
+//------
+
+
+//FUNCTION DECLARATION
+
+// function showMessage(){
+//     alert('Sup yo!');
+// }
+
+// showMessage();
+// showMessage();
+
+
+//----
+
+// function showMessage(from, text) {
+
+//     from = '*' + from + '*'; // make "from" look nicer
+  
+//     alert( from + ': ' + text );
+//   }
+  
+//   let from = "Ann";
+  
+//   showMessage(from, "Hello"); // *Ann*: Hello
+  
+//   // the value of "from" is the same, the function modified a local copy
+//   alert( from ); // Ann
+// //----
+
+
+// let sayHi = function() {
+//     alert( "Hello" );
+//   };
+
+//-----
+// 1/11/20
+
+// let car = {
+//     type: "Masseratti",
+//     model: "Gibli",
+//     color: "Royal Blue",
+//     isExpensive: true,
+//     price: 75000.00,
+//     drive: function( ){
+//         console.log("Now driving my " + car.type + " " + car.color + " " + car.model + " is it expensive? " + car.isExpensive + " it's "+ car.price+ ".");
+
+//     }
+// }
+// // console.log(car.model);
+// console.log(car.drive());
+// //dot
+// car.model = "Toyota";
+// console.log(car.model);
+
+// //square
+// car["model"] = "Honda";
+// console.log(car["model"]);
+
+//-----
+
+class Car {
+    constructor(type, color, model, isExpensive, price){
+        this.type = type;
+        this.color = color;
+        this.model = model;
+        this.isExpensive = isExpensive;
+        this.price = price;
+
+    }
+    drive(){
+        console.log("Now driving my " + this.type + " " + this.color + " " + this.model + " is it expensive? " + this.isExpensive + " it's "+ this.price+ ".");
+    }
+}
+let benz = new Car("Benz","Blue", "Hi", true, 7272727);
+benz.drive();
+let bmw = new Car ("BMW");
+
+
+// Animal Classes create dogs, cat that extend from Animal
+// create insanced of each
+//have name, can eat different things, and that they make noise
+// create in java and javascript
+
+//Study functions
+// prototypical inheritance
+
